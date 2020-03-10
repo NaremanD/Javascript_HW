1dd305d5f2667b12633979a582482b7d5a86f202
Nareman Dabbas <naremands@Naremans-MBP.lan1>
2020-03-10 12:40:11 -0500

    Add UFO-level-1 $ UFO-level-2 files

---
+++ StarterCode/UFO-level-1/static/js/app2.js
@@ -0,0 +1,48 @@
+// from data.js
+const tableData = data;
+
+// select tbody 
+tbody = d3.select("tbody")
+tableData.forEach(function(entry) {
+    const row = tbody.append("tr")
+     Object.entries(entry).forEach(function([key,value]) {
+     row.append("td").text(value)
+     });
+});
+
+//--1--collect the value from the input field
+
+const input = d3.select("input")
+const button = d3.select("#filter-btn");
+inputValues = {};
+const handler = function(){
+// // Prevent the page from refreshing
+    d3.event.preventDefault();
+//--2--filter the data for the data inputed
+    let NewData = tableData
+    const query = d3.select(this).select("input");
+    const queryvalue = query.property("value");
+    const queryid = query.attr("id");
+    //console.log(query);
+    inputValues[queryid] = queryvalue;
+    console.log(inputValues);
+    Object.entries(inputValues).forEach(function([key,value]){
+
+    
+    const NewData = NewData.filter(d => d[key] === value)
+    console.log(NewData);
+    });
+
+    const tbody = d3.select("tbody");
+    tbody.html("");
+//--3--use the foreach to append the data of each cell
+//--4--// loop through table using pbject entries
+    NewData.forEach(function(entry) {
+    const row = tbody.append("tr")
+    Object.entries(entry).forEach(function([key,value]) {
+    row.append("td").text(value);
+    })
+})
+}
+
+button.on("click", handler)
